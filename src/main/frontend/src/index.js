9b678742e22307942b123fa10f588de721a7dcb8
이젠강사 <itdanja@kakao.com>
2022-11-25 11:44:11 +0900

    day75 리액트 4장

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -4,8 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// 1. 사용할 컴포넌트 호출
+// 1. 사용할 컴포넌트 호출 [ import 컴포넌트명 from 파일명 ]
 import Library from './chapter3/Library'
+import Clock from './chapter4/Clock'
 
 // 2. Dom 컨테이너 [ public-> index.html 안에 있는 태그 ]
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -17,12 +18,25 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 //        <App />
 //      </React.StrictMode>
 //    );
-// 2. 기본값 [ Library 컴포넌트를 root 에 렌더링
-    root.render(
-      <React.StrictMode>
-        <Library />
-      </React.StrictMode>
-    );
+// 2.  [ Library 컴포넌트를 root 에 렌더링
+//    root.render(
+//      <React.StrictMode>
+//        <Library />
+//      </React.StrictMode>
+//    );
+
+// 3. [ Clock 컴포넌트를 root 에 렌더링 ]
+    // 1. setInterval 1초마다 렌더링
+        //setInterval( (인수) => { 실행문 } , 밀리초 )
+     setInterval( () => {
+            root.render(
+              <React.StrictMode>
+                <Clock />
+              </React.StrictMode>
+            );
+      } , 1000 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
