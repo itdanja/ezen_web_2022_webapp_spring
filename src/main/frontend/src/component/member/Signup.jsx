1c740ae5ba2a3931447828e01b8226b129e76d5b
이젠강사 <itdanja@kakao.com>
2022-11-28 18:10:09 +0900

    day76 스프링 시큐리티

--- src/main/frontend/src/component/member/Signup.jsx
+++ src/main/frontend/src/component/member/Signup.jsx
@@ -12,8 +12,8 @@ function Signup( props ){ // * 회원가입 컴포넌트 *
         }
         //  @CrossOrigin(origins = "http://localhost:3000")
         axios           // 3. axios 비동기통신 이용한 서버[spring] 통신
-            .post( "http://localhost:8080/member/setmember" ,  info ) // 요청
-            .then( res => { alert( res.data) } )                    // 응답
+            .post( "http://localhost:8080/member/setmember" ,  info )   // 요청
+            .then( res => { alert( res.data) } )                        // 응답
     }
     // 2. 인증코드 요청 함수
     const getauth = () => {   alert(" 클릭 이벤트 ")  }
